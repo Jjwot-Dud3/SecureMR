be0303d72b3e65cb9d69d8760e02099b0484ba48
Gabriel Santana <1085855@est.intec.edu.do>
2021-10-24 19:34:38 -0400

    Testing with fonts and colors

--- secure-mr/src/views/Home.vue
+++ secure-mr/src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>AAAA AAAA AAAA AAAA AAAA</p>
+    <p class="red darken-4 white--text">aikapnvpangva</p>
+    <p class="body-1">aikapnvpangva</p>
+    <p class="caption font-weight-black text-capitalize">aikapnvpangva</p><p>Hola</p>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  
 
-  export default {
-    name: 'Home',
+export default {
+  
 
-    components: {
-      HelloWorld,
-    },
-  }
+
+}
 </script>
