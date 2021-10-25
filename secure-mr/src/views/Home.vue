91e8d83065275ed16eff1eba3cfdb8014bdf080a
Gabriel Santana <1085855@est.intec.edu.do>
2021-10-24 20:58:35 -0400

    Testing buttons

--- secure-mr/src/views/Home.vue
+++ secure-mr/src/views/Home.vue
@@ -1,10 +1,36 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p>AAAA AAAA AAAA AAAA AAAA</p>
-    <p class="red darken-4 white--text">aikapnvpangva</p>
-    <p class="body-1">aikapnvpangva</p>
-    <p class="caption font-weight-black text-capitalize">aikapnvpangva</p><p>Hola</p>
+    <!--<v-btn class="pink white--text">Click</v-btn>
+    <v-btn dark color="pink">Click</v-btn>
+    <v-btn depressed dark color="pink">Click</v-btn>-->
+    <v-btn text color="purple">Click me</v-btn> <!--text is equivalent to flat in previous version-->
+
+     <v-btn
+      dark
+      small
+      color="primary"
+    >
+      <v-icon left dark>
+        mdi-minus
+      </v-icon>
+      <span>Minus</span>
+    </v-btn>
+     <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="primary"
+    >
+      <v-icon dark>
+        mdi-minus
+      </v-icon>
+    </v-btn>
+
+    <v-btn outlined small fab dark color="purple">
+      <v-icon>mdi-monitor-star</v-icon> <!--Has to have mdi first to use the icon-->
+    </v-btn>
   </div>
 </template>
 
