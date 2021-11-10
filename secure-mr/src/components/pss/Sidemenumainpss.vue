e0fad88dfe07ce44d06ac16c5f2437a7aeaf8af1
Gabriel Santana <1085855@est.intec.edu.do>
2021-11-09 23:13:12 -0500

    Adding Sidemenumainpss to components for the interfaces of User PSS

--- secure-mr/src/components/pss/Sidemenumainpss.vue
+++ secure-mr/src/components/pss/Sidemenumainpss.vue
@@ -1,59 +1,68 @@
 <template>
     <div>
-        <v-navigation-drawer permanent expand-on-hover>
+        <v-navigation-drawer absolute permanent expand-on-hover color=primary>
         <v-divider></v-divider>
 
-        <v-list nav dense>
-          <v-list-item link>
+        <v-list>
+          <v-list-item>
             <v-list-item-icon>
-              <v-icon>mdi-home-outline</v-icon>
+              <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
+
             <v-list-item-title>Home</v-list-item-title>
-            
-             <v-menu top :offset-x="offset">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn color="grey darken=1" v-bind="attrs" v-on="on" icon>
-          <v-icon>mdi-chevron-right</v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-      <v-list>
-        <v-list-item>
-          <v-list-item-title>Hola</v-list-item-title>
-        </v-list-item>
+          </v-list-item>
+          <v-list-group
+            v-for="item in items"
+            :key="item.title"
+            v-model="item.active"
+            :prepend-icon="item.action"
+            no-action
+            color="accent"
+          >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" ></v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="child in item.items"
+            :key="child.title"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="child.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
       </v-list>
-      </v-card>
-    </v-menu>
-    
-            <!--<v-btn color="grey darken=1" v-bind="attrs" v-on="on" icon>
-              <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>-->
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-icon>
-              <v-icon>mdi-file-document-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Documentos</v-list-item-title>
-             <v-btn color="grey darken=1" v-bind="attrs" v-on="on" icon>
-              <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-icon>
-              <v-icon>mdi-account-multiple-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Afiliados</v-list-item-title>
-             <v-btn color="grey darken=1" v-bind="attrs" v-on="on" icon>
-              <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>
-          </v-list-item>
-        </v-list>
+
+        
       </v-navigation-drawer>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data:  () => ({
+      items: [
+        
+        {
+          action: 'mdi-file-document-outline',
+          items: [
+            { 
+              title: 'Emitir Documentos' 
+            },
+            { title: 'Consultar Documentos'},
+          ],
+          title: 'Documentos',
+        },
+        {
+          action: 'mdi-account-multiple-outline',
+          items: [{ title: 'Consultar Afiliados' }, {title:'Agregar Afiliados'}],
+          title: 'Afiliados',
+        },
+        
+      ],
+    }),
 }
 </script>
--- secure-mr/src/plugins/vuetify.js
+++ secure-mr/src/plugins/vuetify.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify,{
+    
+});
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#f5f5f5',
+            accent: '#E8458B',
+            anchor: '#8c9eff',
+          },
+        },
+      },
 });
