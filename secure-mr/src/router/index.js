688c97a1112a31a7ac02fa63a6e642428b53aa0f
Angelo González <renatoangelo4@gmail.com>
2021-12-01 17:29:57 -0400

    Document table

--- secure-mr/src/router/index.js
+++ secure-mr/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AffiliateBrowseDocuments from '../views/AffiliateBrowseDocuments'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/affiliate/documents',
+    name: "Consulta de Documentos",
+    component: AffiliateBrowseDocuments
+  }
   //{
     //path: '/about',
     //name: 'About',
