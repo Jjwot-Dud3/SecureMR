e997dc5a9b48ead9db16c3b798b00b71b4e9009f
Filfeni <47361476+Filfeni@users.noreply.github.com>
2021-12-03 16:24:42 -0400

    CreateSelecList Affiliate/Ars

---
+++ secure-mr/src/components/common/CreateSelectListAffiliate.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <v-form>
+                        
+                        <v-card class="my-10" max-width="1500px">
+                            
+                            <div>
+                                <h1 class="text-center">Seleccionar Afiliado</h1>
+                            </div>
+                            <div class="mx-auto">
+                                <v-toolbar class="mx-10 my-5">
+                                    <v-icon>
+                                        mdi-magnify
+                                    </v-icon>
+                                    <v-text-field class=""
+                                        color="p1"
+                                        label="Inserte el nombre del paciente"
+                                    ></v-text-field>
+                                </v-toolbar>
+                                
+                                <v-radio-group>
+                                    <div :key="item.itemName" v-for="item in itemList">
+                                        <v-container class="">
+                                            <v-row align-content="end" justify="center" class="mx-5">
+                                                <v-col class="lg" sm=6>
+                                                    <h3>{{item.itemName}}</h3>
+                                                    <p class="mb-0">{{item.itemSubtitle1}}</p>
+                                                    <p>{{item.itemSubtitle2}}</p>
+                                                </v-col>  
+                                                <v-col class="my-auto pa-0" sm=1>
+                                                    <v-radio color="p1"></v-radio>
+                                                </v-col>
+                                            </v-row>
+                                        </v-container>
+                                        
+                                    </div>
+                                </v-radio-group>
+                            </div>
+                        </v-card>
+                    </v-form>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CreateSelectListArs",
+    props:  {
+        itemList: Array
+    }
+}
+</script>
---
+++ secure-mr/src/views/EmitDocumentArs.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Sidemenumainpss/>
+        <CreateSelectListArs :itemList='itemList'/>
+    </div>
+</template>
+
+<script>
+import Sidemenumainpss from '@/components/pss/Sidemenumainpss'
+import CreateSelectListArs from '@/components/common/CreateSelectListArs'
+
+export default {
+    components:
+    {
+        Sidemenumainpss,
+        CreateSelectListArs
+    },
+    data(){
+        return{
+            title: "Seleccionar ARS",
+            itemList: [{
+                itemName: "Nombre",
+                itemSubtitle1: "Subtitulo 1",
+                itemSubtitle2: "Subtitulo 2"
+            }]
+        }
+        
+    }
+}
+</script>
