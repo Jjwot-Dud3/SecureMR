5373a2f22fc7215ae8575ddad2b2af8b825be21d
Gabriel Santana <1085855@est.intec.edu.do>
2021-12-02 20:11:07 -0500

    Creating HomeCard component; Changing file name from Home.vue - PssHome.vue; Adding references of new file name in router index.js; Dividing code from PssHome.vue

---
+++ secure-mr/src/components/common/HomeCards.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-row class="my-auto">
+                <v-col v-for="n in 3" :key="n" cols="12" md="2">
+                  <v-item>
+                  <v-hover>
+                      <template v-slot:default="{ hover }">
+                        <v-card
+                          class="mx-auto"
+                          max-width="244"
+                        >
+                          <v-layout row justify-center align-center>
+                            <v-icon size="80" color="1">mdi-account-multiple</v-icon>
+                            <v-card-text>
+                              <h2 class="g3--text text-center">
+                                32
+                              </h2>
+                              <p class="g3--text text-center ">
+                                {{username}}
+                              </p>
+                            </v-card-text>
+                          </v-layout>
+                          
+
+                          <v-fade-transition>
+                            <v-overlay
+                              v-if="hover"
+                              opacity=0.70
+                              absolute
+                              color="p1"
+                            >
+                              <v-btn>See more info</v-btn>
+                            </v-overlay>
+                          </v-fade-transition>
+                        </v-card>
+                        
+                      </template>
+                    </v-hover>
+                  </v-item>
+                </v-col>
+              </v-row>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:['username']
+}
+</script>
---
+++ secure-mr/src/views/PssHome.vue
@@ -0,0 +1,40 @@
+<template>
+  <div >
+    
+    <Sidemenumainpss/>
+    <v-container>
+          <template>
+          <v-card>
+          <v-item-group>
+            <v-container>
+              <h2 class="p1--text pa-md-4 mx-lg-auto">
+                General
+              </h2>
+              <HomeCards :username= "general.username"/>
+              <h2 class="p1--text pa-md-4 mx-lg-auto">
+              Archivos
+              </h2>
+              <HomeCards/>
+            </v-container>
+          </v-item-group>
+          
+          </v-card>
+        </template>
+    </v-container>
+  </div>
+  
+</template>
+
+<script>
+import Sidemenumainpss from '@/components/pss/Sidemenumainpss'
+import HomeCards from '@/components/common/HomeCards'
+export default {
+    components:{Sidemenumainpss, HomeCards},
+     data: () => ({
+       general: {
+         username:'matt'
+       }
+    }),
+
+}
+</script>
