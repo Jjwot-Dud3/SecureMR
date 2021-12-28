f37d0b0e3acd440a7f4f3411ac95715172ee19ac
Filfeni <47361476+Filfeni@users.noreply.github.com>
2021-12-28 12:15:17 -0400

    browsing trusted persons added

---
+++ secure-mr/src/views/affiliate/AffiliateBrowseTrustedPersons.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <TrustedPersonsList :items="items"/>
+    </div>
+</template>
+
+<script>
+import TrustedPersonsList from '@/components/affiliate/AffiliateTrustedPersonsList.vue'
+
+export default {
+    components:
+    {
+        TrustedPersonsList,
+    },
+    data(){
+        return{
+            items: [
+            {
+                itemName: "Bella Jones",
+                itemSubtitle1: "Edad 16",
+                itemSubtitle2: "Rol: Paciente"
+            },
+            {
+                itemName: "Bella Jones",
+                itemSubtitle1: "Edad 16",
+                itemSubtitle2: "Rol: Paciente"
+            },
+            {
+                itemName: "Bella Jones",
+                itemSubtitle1: "Edad 16",
+                itemSubtitle2: "Rol: Paciente"
+            },
+            ]
+        }
+        
+    }
+}
+</script>
