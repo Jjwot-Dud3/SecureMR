e6db9ceb27a2b877b1407e2c7971ba5263d9b545
anagparedes <anagparedesg@gmail.com>
2021-12-30 21:31:15 -0400

    add toolbar

---
+++ secure-mr/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    
+    <v-toolbar app clipped-left class="elevation-1" color="white">
+        <v-toolbar-title>
+            <span class="font-weight-light">SecureMR</span>    
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn class="ma-2" small color="purple darken-3" outlined>Solicitar Acceso</v-btn>
+    </v-toolbar>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
