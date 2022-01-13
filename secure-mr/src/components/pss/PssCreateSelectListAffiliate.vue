cea61b6960eca7c0e5f677b6887c85ba60e95ce6
Gabriel Santana <1085855@est.intec.edu.do>
2022-01-13 11:40:55 -0400

    Changes

--- secure-mr/src/components/pss/PssCreateSelectListAffiliate.vue
+++ secure-mr/src/components/pss/PssCreateSelectListAffiliate.vue
@@ -73,7 +73,7 @@ export default {
     name: "PssCreateSelectListAffiliate",
     props:  {
         items: Array
-    }
+    },
     methods: {
         next(){
             this.$router.push('/pss/emitDocumentArs')
